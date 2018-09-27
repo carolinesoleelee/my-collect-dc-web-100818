41e1cefeff436ec087640434f7746e508f6a5add
Solee Lee <carolinesoleelee@gmail.com>
2018-09-27 17:27:52 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,10 @@
 def my_collect(array)
+  collection = []
   i = 0
 
   while i < array.length
-    yield (array[i])
+    collection << yield (array[i])
     i = i + 1
   end
-  array
-end
-
-
-my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']) do |student|
-  if student.start_with?("T")
-    "#{student}"
-  end
+  collection
 end
